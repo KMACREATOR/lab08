02c89c2b857c1eacf331e7d6fcffe16e0a7b98b6
KMACREATOR <agredi2567@gmail.com>
2024-06-04 16:54:09 +0300

    added sources

--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,5 @@
 #include <fstream>
-#include <iostream>
-#include <string>
-
+#include <iostream> 
+#include <streing>
 void print(const std::string& text, std::ofstream& out);
 void print(const std::string& text, std::ostream& out = std::cout);
